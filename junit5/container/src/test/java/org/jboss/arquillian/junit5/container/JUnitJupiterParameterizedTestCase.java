13dd81f8859b732055f6a5e1a0e4cb008b613e36
Marek Skacelik <marek.skacelik@gmail.com>
2026-05-19 20:19:12 +0200

    Add @ParameterizedTest integration test and unit test for container mode

--- junit5/container/src/test/java/org/jboss/arquillian/junit5/container/JUnitJupiterParameterizedTestCase.java
+++ junit5/container/src/test/java/org/jboss/arquillian/junit5/container/JUnitJupiterParameterizedTestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2021 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2026 Red Hat Inc. and/or its affiliates and other contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -16,36 +16,44 @@
  */
 package org.jboss.arquillian.junit5.container;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.Method;
+
+import org.jboss.arquillian.junit5.extension.RunModeEvent;
 import org.jboss.arquillian.test.spi.LifecycleMethodExecutor;
 import org.jboss.arquillian.test.spi.TestMethodExecutor;
 import org.jboss.arquillian.test.spi.TestResult;
 import org.jboss.arquillian.test.spi.TestRunnerAdaptor;
+import org.jboss.arquillian.test.spi.event.suite.TestLifecycleEvent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import org.mockito.invocation.InvocationOnMock;
 
-import java.lang.reflect.Method;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-
-public class JUnitParameterizedTestCase extends JUnitTestBaseClass {
+public class JUnitJupiterParameterizedTestCase extends JUnitTestBaseClass {
 
     @Override
     protected void executeAllLifeCycles(TestRunnerAdaptor adaptor) throws Exception {
         doAnswer(new ExecuteLifecycle()).when(adaptor).beforeClass(any(Class.class), any(LifecycleMethodExecutor.class));
         doAnswer(new ExecuteLifecycle()).when(adaptor).afterClass(any(Class.class), any(LifecycleMethodExecutor.class));
-        doAnswer(new ExecuteLifecycle()).when(adaptor).before(any(Object.class), any(Method.class),
-                any(LifecycleMethodExecutor.class));
-        doAnswer(new ExecuteLifecycle()).when(adaptor).after(any(Object.class), any(Method.class),
-                any(LifecycleMethodExecutor.class));
+        doAnswer(new ExecuteLifecycle()).when(adaptor).before(any(Object.class),
+                any(Method.class), any(LifecycleMethodExecutor.class));
+        doAnswer(new ExecuteLifecycle()).when(adaptor).after(any(Object.class),
+                any(Method.class), any(LifecycleMethodExecutor.class));
         doAnswer(new TestExecuteLifecycle()).when(adaptor).test(any(TestMethodExecutor.class));
+        doAnswer(invocation -> {
+            TestLifecycleEvent event = invocation.getArgument(0);
+            if (event instanceof RunModeEvent) {
+                ((RunModeEvent) event).setRunAsClient(false);
+            }
+            return null;
+        }).when(adaptor).fireCustomLifecycle(any(TestLifecycleEvent.class));
     }
 
     @Test
-    public void shouldReportFailures() throws Exception {
+    public void shouldExecuteParameterizedTestOnceInContainer() throws Exception {
         // given
         TestRunnerAdaptor adaptor = mock(TestRunnerAdaptor.class);
         executeAllLifeCycles(adaptor);
@@ -57,9 +65,11 @@ public class JUnitParameterizedTestCase extends JUnitTestBaseClass {
         Assertions.assertEquals(2, result.getTestsSucceededCount());
         Assertions.assertEquals(2, result.getTestsFailedCount());
         Assertions.assertEquals(0, result.getTestsSkippedCount());
-        assertCycle(0, Cycle.BEFORE_RULE, Cycle.AFTER_RULE, Cycle.BEFORE_CLASS_RULE, Cycle.AFTER_CLASS_RULE);
-        assertCycle(1, Cycle.BEFORE_CLASS, Cycle.AFTER_CLASS);
-        assertCycle(4, Cycle.BEFORE, Cycle.TEST, Cycle.AFTER);
+        verify(adaptor, times(1)).beforeClass(any(Class.class), any(LifecycleMethodExecutor.class));
+        verify(adaptor, times(1)).afterClass(any(Class.class), any(LifecycleMethodExecutor.class));
+        verify(adaptor, times(4)).before(any(Object.class), any(Method.class), any(LifecycleMethodExecutor.class));
+        verify(adaptor, times(4)).after(any(Object.class), any(Method.class), any(LifecycleMethodExecutor.class));
+        verify(adaptor, times(2)).test(any(TestMethodExecutor.class));
     }
 
     public static class TestExecuteLifecycle extends ExecuteLifecycle {
